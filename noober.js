551eaaef27a745d90155766e11b22e33e73e19b3
Christian Nikolopoulos <cdnikolopoulos@gmail.com>
2021-01-30 22:41:39 -0600

    reorder

--- noober.js
+++ noober.js
@@ -7,6 +7,54 @@ async function pageLoaded() {
   
   // 🔥 start here: write code to loop through the rides
   
+
+  function renderProduct(product) {
+    document.querySelector('.products').insertAdjacentHTML('beforeend', `
+      <div class="p-4 w-full md:w-1/2 lg:w-1/3">
+        <div class="border h-full p-4 flex flex-col">
+          <h2 class="text-lg font-bold mb-4">${product.name}</h2>
+          <div class="mb-4">
+            <img src="${product.image}">
+          </div>
+          <div class="mb-4 text-gray-900">${product.description}</div>
+          <div class="mt-auto text-purple-500 text-2xl">$${product.price}</div>
+        </div>
+      </div>
+    `)
+  }
+  
+  async function pageLoaded() {
+    let response = await fetch('https://kiei451.com/api/products.json')
+    let json = await response.json()
+  
+    // writes the returned JSON to the console
+    console.log(json)
+    
+    // start here: write code to loop through the products; each iteration
+    // of the loop should call your renderProduct function
+    // make it work first; then extract to the separate renderProduct
+    // function after it's 100% working without it
+  
+    let products = json.products
+  
+    for (let i = 0; i < products.length; i++) {
+      let eachProduct = products[i]
+      renderProduct(eachProduct)
+    }
+  }
+  
+  window.addEventListener('DOMContentLoaded', pageLoaded)
+  
+
+  
+  
+
+
+
+
+
+
+
 }
 
 window.addEventListener('DOMContentLoaded', pageLoaded)
@@ -50,44 +98,6 @@ if (ride.length > 2) {
 }
 
 
-
-
-function renderProduct(product) {
-  document.querySelector('.products').insertAdjacentHTML('beforeend', `
-    <div class="p-4 w-full md:w-1/2 lg:w-1/3">
-      <div class="border h-full p-4 flex flex-col">
-        <h2 class="text-lg font-bold mb-4">${product.name}</h2>
-        <div class="mb-4">
-          <img src="${product.image}">
-        </div>
-        <div class="mb-4 text-gray-900">${product.description}</div>
-        <div class="mt-auto text-purple-500 text-2xl">$${product.price}</div>
-      </div>
-    </div>
-  `)
-}
-
-async function pageLoaded() {
-  let response = await fetch('https://kiei451.com/api/products.json')
-  let json = await response.json()
-
-  // writes the returned JSON to the console
-  console.log(json)
-  
-  // start here: write code to loop through the products; each iteration
-  // of the loop should call your renderProduct function
-  // make it work first; then extract to the separate renderProduct
-  // function after it's 100% working without it
-
-  let products = json.products
-
-  for (let i = 0; i < products.length; i++) {
-    let eachProduct = products[i]
-    renderProduct(eachProduct)
-  }
-}
-
-window.addEventListener('DOMContentLoaded', pageLoaded)
 
 
 
